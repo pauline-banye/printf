e15fd9d4eb301941c1a05823997685227f67b4d1
pauline banye <paulinebanye@gmail.com>
2022-10-18 20:19:44 +0100

    chmod c files

--- main.h
+++ main.h
@@ -43,8 +43,8 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/* FUNCTIONS */
 
+/* FUNCTIONS */
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
@@ -52,6 +52,7 @@ int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
 
 /* Functions to print numbers */
 int print_int(va_list types, char buffer[],
@@ -69,13 +70,16 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
+
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
+
 /* Funcion to print memory address */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
 
 /* Funciotns to handle other specifiers */
 int get_flags(const char *format, int *i);
@@ -83,13 +87,16 @@ int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
+
 /*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
+
 /*Function to print a string in rot 13*/
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
 
 /* width handler */
 int handle_write_char(char c, char buffer[],
@@ -103,6 +110,7 @@ int write_pointer(char buffer[], int ind, int length,
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
+
 
 /* UTILS */
 int is_printable(char);
